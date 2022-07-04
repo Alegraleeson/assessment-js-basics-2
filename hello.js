fd856858da297a54c8c743c940edb43fe7406c6b
Alegraleeson <wingmonkee@yahoo.com>
2022-07-03 22:44:38 -0500

    first commit

---
+++ hello.js
@@ -0,0 +1,19 @@
+console.log(`You're ready to begin!`)
+
+// Traditional Anonymous Function (arrow function)
+function (a){
+    return a + 100;
+  }
+  
+  // Arrow Function Break Down
+  
+  // 1. Remove the word "function" and place arrow between the argument and opening body bracket
+  (a) => {
+    return a + 100;
+  }
+  
+  // 2. Remove the body braces and word "return" — the return is implied.
+  (a) => a + 100;
+  
+  // 3. Remove the argument parentheses
+  a => a + 100;
